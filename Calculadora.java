67875bc33ff79975ace20389bfb4df21765a1539
elmerhomero53 <53914081+elmerhomero53@users.noreply.github.com>
2020-02-03 16:14:37 -0600

    ultimos metodos calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -41,6 +41,54 @@ public class Calculadora {
             br.close();
         }
     }
+
+    private int calculate(String input) {
+        Stack_<Integer> stack = new Stack_<>();
+
+        String[] inputs = input.split(" ");
+
+        return handleCalculation(stack, inputs);
+    }
+
+    private static int handleCalculation(Stack_<Integer> stack, String[] el) {
+        int operand1, operand2;
+
+        for(int i = 0; i < el.length; i++) {
+            if( el[i].equals(ADD) || el[i].equals(SUB) || el[i].equals(MUL) || el[i].equals(DIV) ) {
+                operand2 = stack.pop();
+                operand1 = stack.pop();
+                switch(el[i]) {
+                    case ADD: {
+                        int local = operand1 + operand2;
+                        stack.push(local);
+                        break;
+                    }
+
+                    case SUB: {
+                        int local = operand1 - operand2;
+                        stack.push(local);
+                        break;
+                    }
+
+                    case MUL: {
+                        int local = operand1 * operand2;
+                        stack.push(local);
+                        break;
+                    }
+
+                    case DIV: {
+                        int local = operand1 / operand2;
+                        stack.push(local);
+                        break;
+                    }
+                }
+            } else {
+                stack.push(Integer.parseInt(el[i]));
+            }
+        }
+
+        return stack.pop();
+    }
     
 }
 
