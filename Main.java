1c8a3ad793b4a7c9c7b37fa7de3e04389e196a12
elmerhomero53 <53914081+elmerhomero53@users.noreply.github.com>
2020-02-03 15:47:03 -0600

    Main y clase stack

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+//Hoja de trabajo 3
+//Daniela Batz
+/*Jose Ramos
+Algoritmos y estructuras de datos
+Calculadora*/
+
+public class Main {
+    public static void main(String[] args){
+        Calculadora calc = new Calculadora	(); //instanciamos la calculadora
+        calc.calculateFile("datos.txt"); //usamos el metodo que realiza la operacion
+    }
+}	
---
+++ Stack.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Stack {
+    private int size;
+    private Node<T> head;
+
+    public Stack(){
+        size=0;
+        head = null;
+    }
+}
