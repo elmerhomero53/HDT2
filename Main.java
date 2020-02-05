3f55f613802ed81833f5463837930545a2ce4a9e
DanBea2019 <53918887+DanBea2019@users.noreply.github.com>
2020-02-04 21:32:41 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
-package com.company;
-import java.io.IOException;
-
-/*Hoja de trabajo 3
-Archivo main
-Daniela Batz
-Jose Ramos
-Algoritmos y estructuras de datos
-Calculadora*/
-
+/**
+ * 
+ * @author Daniela Batz
+ * @author Jose Ramos
+ *
+ */
 public class Main {
-    public static void main(String[] args){
+	public static void main(String[] args){
         Calculadora calc = new Calculadora	(); //instanciamos la calculadora
         calc.calculateFile("datos.txt"); //usamos el metodo que realiza la operacion
     }
-}	
+}
